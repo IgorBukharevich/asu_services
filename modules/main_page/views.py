9a22478b44a32c20c4434faddf2066e608618f20
Igor <igor.bukharevich.lifanor@gmail.com>
2024-12-23 15:03:31 +0300

    created Main_page, add urls app_main_page, edit views.py to pull data from a database onto an html template

--- modules/main_page/views.py
+++ modules/main_page/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import TaskMPAsu, TitleMainPage
+
+
+def view_title(request):
+    """give_s is models TitleMainPage pk=1(title)
+       give_S is models TaskMPAsu all objects
+    """
+    # TODO: задачи можно будет вытянуть из другого АПП на главную страницу. Так же там могут находится и О НАС КОНТАКТЫ
+    title = TitleMainPage.objects.get(pk=1)
+    tasks = TaskMPAsu.objects.all()
+    return render(
+        request,
+        template_name='main_page/main_page.html',
+        context={'title_asu': title, 'tasks_asu': tasks}
+    )
