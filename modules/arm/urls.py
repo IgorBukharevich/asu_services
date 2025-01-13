05daea7509ef1e43ec930034974dc53b637bc457
Igor <igor.bukharevich.lifanor@gmail.com>
2025-01-13 09:18:13 +0300

    Добавлена страница с детальным описанием АРМ

--- modules/arm/urls.py
+++ modules/arm/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ArmListView
+from .views import ArmListView, ArmDetailView
 
 
 urlpatterns = [
     path('arm_list/', ArmListView.as_view(), name='arm_list'),
+    path('arm_detail/<str:slug>', ArmDetailView.as_view(), name='arm_detail'),
 ]
--- modules/arm/views.py
+++ modules/arm/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from .models import Arm, Department
 
@@ -13,4 +13,15 @@ class ArmListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Список АРМов'
+        return context
+
+
+class ArmDetailView(DeleteView):
+    model = Arm
+    template_name = 'arm/arm_detail.html'
+    context_object_name = 'arm_detail'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = self.object.num_arm
         return context
